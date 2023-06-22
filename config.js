48639fb6fb6941508c3719c7165d8400b8f405df
takk2050 <94603328@qq.com>
2023-06-22 19:42:27 +0800

    update .gitignore

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "pin": "599965",
+  "client_id": "5a03cd4c-831a-46c3-ae2b-0d8769284351",
+  "session_id": "12718001-eb20-46b4-a8eb-d43d24077155",
+  "pin_token": "z8KPtn2Jdlf58Aa1Hay3ImzoiRzGyU7rxPFkIB2ngD0",
+  "private_key": "9huJX4pdvixm_6OwK-8pYXNrleSEZjZ4Go-2e5VJoA_54LokAQ6LaoHxWCTVZFtoLXIsIqEBPLRW45JyAYAQRw"
+   , "openai_key": "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",
+};
+
